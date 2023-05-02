8019b3c93433ff11f610d8b1bb9a473a0abf69c6
Lucas1774 <lucas.115@hotmail.com>
2023-05-02 20:51:21 +0200

    First success

--- 2/StockControl.hs
+++ 2/StockControl.hs
@@ -3,39 +3,89 @@ module StockControl where
 data Stock = ROOTNODE [Stock] | INNERNODE Char [Stock] | INFONODE Int
   deriving (Show,Read,Eq)
 
+-------------------------
+-- FUNCIÓN CREATESTOCK --
+-------------------------
+
+-- FUNCIÓN QUE DEVUELVE UN STOCK VACÍO --
 createStock :: Stock
 createStock = ROOTNODE[]
 
+
+---------------------------
+-- FUNCIÓN RETRIEVESTOCK --
+---------------------------
+
+-- FUNCIÓN QUE DEVUELVE EL NÚMERO DE UNIDADES DE UN PRODUCTO EN EL STOCK --
+-- SI NO ESTÁ, DEBERÁ DEVOLVER -1                                        --
+
 retrieveStock :: Stock -> String -> Int
-retrieveStock (ROOTNODE []) _ = -1
-retrieveStock (ROOTNODE children) string = faux children string
+retrieveStock (ROOTNODE []) _ = -1 --empty -> do not bother
+retrieveStock (ROOTNODE children) string = retrieveStock children string --not empty -> operate with children
   where
-    faux :: [Stock] -> String -> Int
-    faux (INFONODE n:[]) "" = n
-    faux (INNERNODE c children:rest) (x:xs)
-      | c == x = faux children xs
-      | otherwise = faux rest (x:xs)
-    faux _ _ = -1
+    retrieveStock :: [Stock] -> String -> Int
+    retrieveStock (INFONODE n:[]) "" = n --found end -> get value
+    retrieveStock (INNERNODE c children:rest) (x:xs) --inner node -> check if match
+      | c == x = retrieveStock children xs --found match -> go deeper
+      | c < x = retrieveStock rest (x:xs) --explore level -> go wider
+      | otherwise = -1 --not found
+    retrieveStock _ _ = -1 --default not found
+
+
+-------------------------
+-- FUNCIÓN UPDATESTOCK --
+-------------------------
+
+-- FUNCIÓN QUE MODIFICA EL VALOR ASOCIADO A UN PRODUCTO EN EL STOCK --
+-- SÓLO PUEDE ALMACENAR NÚMEROS MAYORES O IGUALES A 0               --
 
 updateStock :: Stock -> String -> Int -> Stock
-updateStock (ROOTNODE children) str val = ROOTNODE (faux children str val)
+updateStock (ROOTNODE children) string value = ROOTNODE (updateStock' children string value) --operate with children
   where
-    faux :: [Stock] -> String -> Int -> [Stock]
-    faux [] "" val = [INFONODE val]
-    faux [] (x:xs) val = [INNERNODE x (faux [] xs val)]
-    faux (INFONODE _ : rest) "" val = INFONODE val : rest
-    faux (INFONODE _ : rest) (x:xs) val = INFONODE val : faux rest (x:xs) val
-    faux (INNERNODE c children : rest) (x:xs) val
-      | c == x = INNERNODE c (faux children xs val) : rest
-      | otherwise = INNERNODE c children : faux rest (x:xs) val
-    faux (node : rest) str val = node : faux rest str val
+    updateStock' :: [Stock] -> String -> Int -> [Stock]
+    updateStock' [] "" value = [INFONODE value] --found end -> create infonode
+    updateStock' (INFONODE _ : rest) "" value = INFONODE value : rest --a different end -> update infonode, keep siblings-
+    updateStock' [] (x:xs) value = [INNERNODE x (updateStock' [] xs value)] --dead end -> create innernode and go deeper
+    updateStock' (INNERNODE c children : rest) (x:xs) value --inner node -> check if match
+      | c == x = INNERNODE c (updateStock' children xs value) : rest --found match -> go deeper
+      | c < x = INNERNODE c children : updateStock' rest (x:xs) value --explore level -> go wider
+      | otherwise = INNERNODE x (updateStock' [] xs value) : INNERNODE c children : rest --not found -> create innernode and go deeper
+    updateStock' (node : rest) string value = node : updateStock' rest string value --default not found
 
 
+-----------------------
+-- FUNCIÓN LISTSTOCK --
+-----------------------
 
--- in this function String is a prefix --
+-- FUNCIÓN QUE DEVUELVE UNA LISTA PARES PRODUCTO-EXISTENCIA --
+-- DEL CATÁLOGO QUE COMIENZAN POR LA CADENA PREFIJO p       --
 listStock :: Stock -> String -> [(String, Int)]
-listStock s string = [("a",2)]
--- generic backtracking function --
+listStock stock string = toTuples(bt isSolution children (get stock string, string))
+  where
+    toTuples nodes = concatMap toTuples' nodes
+      where
+        toTuples' (INFONODE i, s) = [(s, i)]
+
+    isSolution (INFONODE _, _) = True --infonodes are solution because filtering is done beforehand
+    isSolution _ = False
+
+    children (ROOTNODE next, s) = [(child, s) | child <- next] --create tuples with string s
+    children (INNERNODE c next, s) = [(child, s ++ [c]) | child <- next] -- create tuples with string s + c
+    children _ = []
+
+    get (ROOTNODE []) _ = ROOTNODE [] --empty -> return empty
+    get (ROOTNODE children) string = ROOTNODE (get' children string) --not empty -> return new with children
+      where
+        get' :: [Stock] -> String -> [Stock]
+        get' nodes "" = nodes --found end -> return children
+        get' (INNERNODE char first:rest) (x:xs) --inner node -> check if match
+          | char == x = get' first xs --found match -> go deeper
+          | char < x = get' rest (x:xs) --explore level -> go wider
+          | otherwise = [] --not found -> return empty
+        get' _ _ = [] --default return empty
+
+
+-- FUNCIÓN GENÉRICA DE BACKTRACKING --
 bt :: (a -> Bool) -> (a -> [a]) -> a -> [a]
 bt    eS             c             n
   | eS n      = [n]
